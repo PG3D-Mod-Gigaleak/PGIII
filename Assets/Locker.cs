9d97746ab482bb35b393d5076b78201e03f2da98
EnderPurrson <enderlord9990@gmail.com>
2024-01-25 14:45:48 -0500

    whelp

--- Assets/Locker.cs
+++ Assets/Locker.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Locker : MonoBehaviour
 {
+	void Awake()
+	{
+		Resources.Load<ServerSettings>("PhotonServerSettings").UseCloud("83abbecb-d18d-4f34-9b4f-2f024e5a5993", 0);
+	}
+
 	void Start()
 	{
 		keychainPlugin.updateKCValue(999999, Defs.Coins);
