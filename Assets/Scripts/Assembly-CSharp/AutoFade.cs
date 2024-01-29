13ecd0e84fd1fade2d0b9e8f1ec6709fbd98f4ee
EnderPurrson <enderlord9990@gmail.com>
2024-01-28 23:28:55 -0500

    fadeout fix

--- Assets/Scripts/Assembly-CSharp/AutoFade.cs
+++ Assets/Scripts/Assembly-CSharp/AutoFade.cs
@@ -37,7 +37,7 @@ public class AutoFade : MonoBehaviour
 	{
 		Object.DontDestroyOnLoad(this);
 		m_Instance = this;
-		m_Material = new Material("Shader \"Plane/No zTest\" { SubShader { Pass { Blend SrcAlpha OneMinusSrcAlpha ZWrite Off Cull Off Fog { Mode Off } BindChannels { Bind \"Color\",color } } } }");
+		m_Material = new Material(Resources.Load<Shader>("NozTest"));
 	}
 
 	private void DrawQuad(Color aColor, float aAlpha)
