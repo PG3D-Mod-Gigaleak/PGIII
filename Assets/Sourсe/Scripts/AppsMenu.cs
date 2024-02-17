3ab2e630d8ea4437ae04e0e2bd1309c4a610cde8
zUltimaPG3D <ggyuuhallo233@proton.me>
2024-02-17 16:28:13 +0100

    chore(realism): reorganize almost everything to make it seem more like it's a real project and not an assetripper export

---
+++ Assets/Sourсe/Scripts/AppsMenu.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class AppsMenu : MonoBehaviour {
+	public Texture fon;
+
+	public Texture pixlgun3d;
+
+	public Texture man;
+
+	public Texture androidFon;
+
+	public GUIStyle shooter;
+
+	public GUIStyle skinsmaker;
+
+	private void Update()
+	{
+		if (Application.platform == RuntimePlatform.Android && Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+	private void Start()
+	{
+#if UNITY_EDITOR
+        Invoke("LoadLoading", 1f/30f);
+#else
+        Invoke("LoadLoading", 0.1f);
+#endif
+    }
+
+    private void LoadLoading()
+	{
+		GlobalGameController.currentLevel = -1;
+        Application.LoadLevel("Loading");
+    }
+
+    private void OnGUI()
+	{
+		Rect position = new Rect(((float)Screen.width - 2048f * (float)Screen.height / 1154f) / 2f, 0f, 2048f * (float)Screen.height / 1154f, Screen.height);
+		GUI.DrawTexture(position, androidFon, ScaleMode.StretchToFill);
+	}
+}
